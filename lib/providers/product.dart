d10f3e3e86882d9d87841f3d7e60058ec45b4a87
php1301 <luudanthanchet@gmail.com>
2020-09-26 20:41:04 +0700

    Product Provider added

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
-class Product {
+class Product with ChangeNotifier {
   final String id;
   final String title;
   final String description;
@@ -16,4 +16,8 @@ class Product {
     @required this.imageUrl,
     this.isFavorite = false,
   });
+  void toggleFavoriteStatus() {
+    isFavorite = !isFavorite;
+    notifyListeners(); //  giong setState nhung chi goi den cac listeners
+  }
 }
