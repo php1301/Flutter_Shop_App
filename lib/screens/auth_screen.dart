9b3910ae22ce9067ced9b978e679298a8222b01a
php1301 <luudanthanchet@gmail.com>
2020-09-28 09:39:25 +0700

    AnimatedContainer for simple animation like changing size container

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -106,28 +106,22 @@ class _AuthCardState extends State<AuthCard>
   var _isLoading = false;
   final _passwordController = TextEditingController();
   AnimationController _controller;
-  Animation<Size> _heightAnimation;
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-        vsync: this,
-        duration: Duration(
-          milliseconds: 300,
-        ));
-    _heightAnimation = Tween<Size>(
-            begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
-        .animate(CurvedAnimation(
-            parent: _controller,
-            curve: Curves.linear)); //Hieu ung Curve va controller
-    // _heightAnimation.addListener(() => setState(() {}));
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
-  }
+  // Animation<Size> _heightAnimation;
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _controller = AnimationController(
+  //       vsync: this,
+  //       duration: Duration(
+  //         milliseconds: 300,
+  //       ));
+  //   _heightAnimation = Tween<Size>(
+  //           begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
+  //       .animate(CurvedAnimation(
+  //           parent: _controller,
+  //           curve: Curves.linear)); //Hieu ung Curve va controller
+  //   _heightAnimation.addListener(() => setState(() {}));
+  // }
 
   void _showErrorDialog(String message) {
     showDialog(
@@ -209,18 +203,16 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (ctx, ch) => Container(
-          // height: _authMode == AuthMode.Signup ? 320 : 260,
-          height: _heightAnimation.value.height,
-          constraints:
-              // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-              BoxConstraints(minHeight: _heightAnimation.value.height),
-          width: deviceSize.width * 0.75,
-          padding: EdgeInsets.all(16.0),
-          child: ch,
-        ),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        // height: _heightAnimation.value.height,
+        constraints:
+            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        // BoxConstraints(minHeight: _heightAnimation.value.height),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
